021e1b3eb4680aae10f9e6985379b255f221bd03
Koldo Picaza <dev@antidotfw.io>
2021-04-05 16:52:10 +0200

    add license

--- src/InMemoryFeatureFactory.php
+++ src/InMemoryFeatureFactory.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Pheature\InMemory\Toggle;
 
 use Pheature\Core\Toggle\Read\Feature as IFeature;
+use Pheature\Core\Toggle\Read\Segments;
 use Pheature\Core\Toggle\Read\ToggleStrategies;
+use Pheature\Core\Toggle\Read\ToggleStrategy;
+use Pheature\Model\Toggle\EnableByMatchingSegment;
 use Pheature\Model\Toggle\Feature;
+use Pheature\Model\Toggle\Segment;
 
 final class InMemoryFeatureFactory
 {
@@ -14,8 +18,20 @@ final class InMemoryFeatureFactory
     {
         return new Feature(
             $data['id'],
-            new ToggleStrategies(...$data['strategies']),
+            new ToggleStrategies(...array_map([$this, 'makeStrategy'], $data['strategies'])),
             $data['enabled']
+        );
+    }
+
+    private static function makeStrategy(array $strategy): ToggleStrategy
+    {
+        return new EnableByMatchingSegment(
+            new Segments(
+                ...array_map(
+                    static fn(array $segment): Segment => new Segment($segment['id'], $segment['criteria']),
+                    $strategy['segments']
+                )
+            )
         );
     }
 }
