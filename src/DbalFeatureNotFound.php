f71f234f663d33943f047c1b242de2c8845ad584
Koldo Picaza <dev@antidotfw.io>
2021-04-06 20:57:08 +0200

    implement feature not found exception

--- src/DbalFeatureNotFound.php
+++ src/DbalFeatureNotFound.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Pheature\Dbal\Toggle;
 
 use InvalidArgumentException;
+use Pheature\Core\Toggle\Exception\FeatureNotFoundException;
 
-final class DbalFeatureNotFound extends InvalidArgumentException
+final class DbalFeatureNotFound extends InvalidArgumentException implements FeatureNotFoundException
 {
     public static function withId(string $featureId): self
     {
