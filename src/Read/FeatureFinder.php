221975d9f9e09cc59f4ecf97c7b0cbfa047e2261
Koldo Picaza <dev@antidotfw.io>
2021-04-06 20:55:34 +0200

    add feature not found exception interface

--- src/Read/FeatureFinder.php
+++ src/Read/FeatureFinder.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Pheature\Core\Toggle\Read;
 
+use Pheature\Core\Toggle\Exception\FeatureNotFoundException;
+
 interface FeatureFinder
 {
+    /**
+     * @param string $featureId
+     * @return Feature
+     * @throws FeatureNotFoundException
+     */
     public function get(string $featureId): Feature;
 
     /**
