44b121f236571db53c69fd04096e65bafdd1ff53
Koldo Picaza <dev@antidotfw.io>
2021-04-10 14:24:14 +0200

    add basic functionality

---
+++ src/DeleteFeature.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pheature\Crud\Psr7\Toggle;
+
+use Pheature\Crud\Toggle\Command\RemoveFeature as RemoveFeatureCommand;
+use Pheature\Crud\Toggle\Handler\RemoveFeature;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class DeleteFeature implements RequestHandlerInterface
+{
+    private RemoveFeature $removeFeature;
+    private ResponseFactoryInterface $responseFactory;
+
+    public function __construct(RemoveFeature $removeFeature, ResponseFactoryInterface $responseFactory)
+    {
+        $this->removeFeature = $removeFeature;
+        $this->responseFactory = $responseFactory;
+    }
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->removeFeature->handle(
+            RemoveFeatureCommand::withId($request->getAttribute('feature_id'))
+        );
+
+        return $this->responseFactory->createResponse(204, 'Deleted');
+    }
+}
+    
