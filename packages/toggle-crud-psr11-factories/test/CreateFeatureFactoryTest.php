f5303b49685a0d8423f931b8858bd2ee0390a086
paudelacuesta <paudelacuesta@ciclion.com>
2021-04-20 23:43:20 +0200

    [#124] CreateFeatureFactory Tests

---
+++ packages/toggle-crud-psr11-factories/test/CreateFeatureFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pheature\Test\Crud\Psr11\Toggle;
+
+use Pheature\Core\Toggle\Write\FeatureRepository;
+use Pheature\Crud\Psr11\Toggle\CreateFeatureFactory;
+use Pheature\Crud\Toggle\Handler\CreateFeature;
+use Psr\Container\ContainerInterface;
+use PHPUnit\Framework\TestCase;
+
+class CreateFeatureFactoryTest extends TestCase
+{
+    public function testItShouldCreateInstanceOfCreateFeature(): void
+    {
+        $featureRepository = $this->createMock(FeatureRepository::class);
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects(static::once())
+            ->method('get')
+            ->with(FeatureRepository::class)
+            ->willReturn($featureRepository);
+
+        $addStrategyFactory = new CreateFeatureFactory();
+
+        $addStrategyFactory->__invoke($container);
+    }
+
+    public function testItShouldCreateInstanceOfCreateFeatureStatically(): void
+    {
+        $featureRepository = $this->createMock(FeatureRepository::class);
+
+        $addStrategy = CreateFeatureFactory::create($featureRepository);
+
+        $this->assertInstanceOf(CreateFeature::class, $addStrategy);
+    }
+}
