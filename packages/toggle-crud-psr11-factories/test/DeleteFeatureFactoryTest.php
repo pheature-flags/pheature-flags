aded60a94f899884b308d0c4d49ad2ea5c0bc39c
paudelacuesta <paudelacuesta@ciclion.com>
2021-04-21 22:28:47 +0200

    [#125] DeleteFeatureFactory Static + Test

---
+++ packages/toggle-crud-psr11-factories/test/DeleteFeatureFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Pheature\Test\Crud\Psr11\Toggle;
+
+use Pheature\Core\Toggle\Write\FeatureRepository;
+use Pheature\Crud\Psr11\Toggle\DeleteFeatureFactory;
+use Pheature\Crud\Psr7\Toggle\DeleteFeature;
+use Pheature\Crud\Toggle\Handler\RemoveFeature;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+class DeleteFeatureFactoryTest extends TestCase
+{
+    public function testItShouldCreateInstanceOfDeleteFeature(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $responseFactory = $this->createMock(ResponseFactoryInterface::class);
+        $featureRepository = $this->createMock(FeatureRepository::class);
+
+        $removeFeature = new RemoveFeature($featureRepository);
+
+        $container->expects(static::exactly(2))
+            ->method('get')
+            ->withConsecutive([RemoveFeature::class], [ResponseFactoryInterface::class])
+            ->willReturnOnConsecutiveCalls($removeFeature, $responseFactory);
+
+        $deleteFeatureFactory = new DeleteFeatureFactory();
+
+        $deleteFeatureFactory->__invoke($container);
+    }
+
+    public function testItShouldCreateInstanceOfDeleteFeatureStatically(): void
+    {
+        $featureRepository = $this->createMock(FeatureRepository::class);
+        $responseFactory = $this->createMock(ResponseFactoryInterface::class);
+        $removeFeature = new RemoveFeature($featureRepository);
+
+        $createdInstance = DeleteFeatureFactory::create($removeFeature, $responseFactory);
+
+        $this->assertInstanceOf(DeleteFeature::class, $createdInstance);
+    }
+}
