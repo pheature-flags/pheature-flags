0faca6ba6e469fd81ed469cedfbac6fc3ccc9ded
xserrat <xavier.serrat22@gmail.com>
2021-04-21 21:28:53 +0200

    issue-135 [Toggle PSR-11 Factories] Add RemoveStrategyFactoryTest

---
+++ packages/toggle-crud-psr11-factories/test/RemoveStrategyFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pheature\Test\Crud\Psr11\Toggle;
+
+use Pheature\Core\Toggle\Write\FeatureRepository;
+use Pheature\Crud\Psr11\Toggle\RemoveStrategyFactory;
+use Pheature\Crud\Toggle\Handler\RemoveStrategy;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+class RemoveStrategyFactoryTest extends TestCase
+{
+    public function testItShouldCreateARemoveStrategyFromInvokable(): void
+    {
+        $mockedContainer = $this->createConfiguredMock(ContainerInterface::class, [
+            'get' => $this->createMock(FeatureRepository::class)
+        ]);
+
+        $factory = new RemoveStrategyFactory();
+        $actual = $factory->__invoke($mockedContainer);
+
+        self::assertInstanceOf(RemoveStrategy::class, $actual);
+    }
+
+    public function testItShouldCreateARemoveStrategyFromCreate(): void
+    {
+        $actual = RemoveStrategyFactory::create($this->createMock(FeatureRepository::class));
+
+        self::assertInstanceOf(RemoveStrategy::class, $actual);
+    }
+}
