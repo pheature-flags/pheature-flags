b4839062e97c65249ddf6b4280fbc0f2e913363b
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-04-26 21:39:03 +0200

    [#169] create toggle strategy factory compiler pass

--- packages/symfony-toggle/src/DependencyInjection/Configuration.php
+++ packages/symfony-toggle/src/DependencyInjection/Configuration.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace Pheature\Community\Symfony\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
+    /**
+     * @return TreeBuilder
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('pheature_flags');
-
+        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
+        /** @phpstan-ignore-next-line */
         $rootNode->children()
             ->enumNode('driver')
                 ->values(['inmemory', 'dbal'])
--- packages/symfony-toggle/src/PheatureFlagsBundle.php
+++ packages/symfony-toggle/src/PheatureFlagsBundle.php
@@ -4,43 +4,49 @@ declare(strict_types=1);
 
 namespace Pheature\Community\Symfony;
 
+use Pheature\Community\Symfony\DependencyInjection\ToggleStrategyFactoryPass;
 use Pheature\Model\Toggle\EnableByMatchingIdentityId;
 use Pheature\Model\Toggle\EnableByMatchingSegment;
 use Pheature\Model\Toggle\IdentitySegment;
-use Pheature\Model\Toggle\Segment;
 use Pheature\Model\Toggle\SegmentFactory;
 use Pheature\Model\Toggle\StrategyFactory;
+use Pheature\Model\Toggle\StrictMatchingSegment;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class PheatureFlagsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    private const DEFAULT_CONFIG = [
+        'driver' => 'inmemory',
+        'prefix' => '',
+        'segment_types' => [
+            [
+                'type' => IdentitySegment::NAME,
+                'factory_id' => SegmentFactory::class
+            ],
+            [
+                'type' => StrictMatchingSegment::NAME,
+                'factory_id' => SegmentFactory::class
+            ]
+        ],
+        'strategy_types' => [
+            [
+                'type' => EnableByMatchingSegment::NAME,
+                'factory_id' => StrategyFactory::class
+            ],
+            [
+                'type' => EnableByMatchingIdentityId::NAME,
+                'factory_id' => StrategyFactory::class
+            ]
+        ],
+        'toggles' => [],
+    ];
+
+    public function build(ContainerBuilder $container): void
     {
-        $container->loadFromExtension('pheature_flags', [
-            'driver' => 'inmemory',
-            'prefix' => '',
-            'segment_types' => [
-                [
-                    'type' => IdentitySegment::NAME,
-                    'factory_id' => SegmentFactory::class
-                ],
-                [
-                    'type' => Segment::NAME,
-                    'factory_id' => SegmentFactory::class
-                ]
-            ],
-            'strategy_types' => [
-                [
-                    'type' => EnableByMatchingSegment::NAME,
-                    'factory_id' => StrategyFactory::class
-                ],
-                [
-                    'type' => EnableByMatchingIdentityId::NAME,
-                    'factory_id' => StrategyFactory::class
-                ]
-            ],
-            'toggles' => [],
-        ]);
+        $container->loadFromExtension('pheature_flags', self::DEFAULT_CONFIG);
+
+        $toggleStrategyFactoryPass = new ToggleStrategyFactoryPass();
+        $toggleStrategyFactoryPass->process($container);
     }
 }
