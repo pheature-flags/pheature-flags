946d313224e6a146cbf4922d3b98c5bff6881e3a
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-05-24 22:51:09 +0200

    HOTFIX: add consistency on serializing segment criteria

--- packages/toggle-core/src/Write/Segment.php
+++ packages/toggle-core/src/Write/Segment.php
@@ -42,7 +42,7 @@ final class Segment implements JsonSerializable
         return [
             'segment_id' => $this->segmentId->value(),
             'segment_type' => $this->segmentType->value(),
-            'payload' => $this->payload->criteria(),
+            'criteria' => $this->payload->criteria(),
         ];
     }
 }
