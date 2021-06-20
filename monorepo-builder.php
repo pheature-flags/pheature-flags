968859162eb602eaff3a867e6924be6904a1dd22
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-06-20 02:09:21 +0200

    [0.1.1] generate automatic changelog

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -27,7 +27,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(AddTagToChangelogReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
 };
